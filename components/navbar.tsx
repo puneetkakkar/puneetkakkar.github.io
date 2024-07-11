772e807c823c0fc77bebb9085443ffd51143cb29
Puneet Kakkar <puneetkakkar91@gmail.com>
2024-07-11 16:51:48 -0400

    add nextui with dark and light theme

---
+++ components/navbar.tsx
@@ -0,0 +1,102 @@
+import {
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Navbar as NextUINavbar,
+} from "@nextui-org/navbar";
+import { link as linkStyles } from "@nextui-org/theme";
+import clsx from "clsx";
+import NextLink from "next/link";
+
+import { ThemeSwitch } from "@/components/theme-switch";
+import { siteConfig } from "@/config/site";
+
+export const Navbar = () => {
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink
+            className="flex justify-start items-center gap-1 font-rubik text-large font-extrabold bg-[linear-gradient(105.36deg,_#0052F4_0%,_#0052F4_76.61%,_#0054F5_150.11%)] text-transparent bg-clip-text"
+            href="/"
+          >
+            Puneet Kakkar
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+        <ul className="hidden sm:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium font-inter text-sm m-2"
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+        {/* {showAuthButtons ? (
+          <>
+            <NavbarItem className="hidden md:flex">
+              <Button
+                as={NextLink}
+                href="/login"
+                color="primary"
+                variant="flat"
+                className="font-normal font-rubik"
+              >
+                Login
+              </Button>
+            </NavbarItem>
+            <NavbarItem className="hidden md:flex">
+              <Button
+                as={NextLink}
+                href="/signup"
+                className="font-rubik font-normal text-primary-foreground bg-primary"
+                variant="flat"
+              >
+                Sign up
+              </Button>
+            </NavbarItem>
+          </>
+        ) : null} */}
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {siteConfig.navItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <NextLink
+                color={index === 0 ? "primary" : "foreground"}
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </NextUINavbar>
+  );
+};
